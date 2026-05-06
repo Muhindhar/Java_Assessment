dd12e1d137d1240afe0aec39299d287a8a2f3979
Muhindhar <muhindharsv27@gmail.com>
2026-05-06 15:51:26 +0530

    Added assessment projects

---
+++ Assessment4/src/Test4/Dao.java
@@ -0,0 +1,68 @@
+package Test4;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.PreparedStatement;
+
+public class Dao {
+
+    // 3.1 Retrieve all books
+    public void getAllBooks() {
+
+        try (Connection conn = DbConnection.getConnection();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery("SELECT * FROM book")) {
+
+            System.out.println("\n--- BOOKS ---");
+
+            while (rs.next()) {
+                System.out.println("ID: " + rs.getInt("bookid")); // match SQL
+                System.out.println("Title: " + rs.getString("title"));
+                System.out.println("Author: " + rs.getString("author"));
+                System.out.println("----------------------");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // 3.2 Update fine and display
+    public void updateFine() {
+
+        try (Connection conn = DbConnection.getConnection()) {
+
+            // UPDATE
+            String updateQuery = "UPDATE members SET fine = 500 " +
+                    "WHERE membership_type = 'Basic' AND status = 'Overdue'";
+
+            PreparedStatement ps = conn.prepareStatement(updateQuery);
+            int rows = ps.executeUpdate();
+
+            System.out.println("\nRows Updated: " + rows);
+
+            // SELECT updated records
+            String selectQuery = "SELECT firstname, lastname, membership_type, status, fine " +
+                    "FROM members WHERE membership_type = 'Basic' AND status = 'Overdue'";
+
+            ps = conn.prepareStatement(selectQuery);
+            ResultSet rs = ps.executeQuery();
+
+            System.out.println("\n--- Updated Members ---");
+
+            while (rs.next()) {
+                System.out.println(
+                        rs.getString("firstname") + " " +
+                                rs.getString("lastname") + " | " +
+                                rs.getString("membership_type") + " | " +
+                                rs.getString("status") + " | Fine: " +
+                                rs.getInt("fine")
+                );
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
